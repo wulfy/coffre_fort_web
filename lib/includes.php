ee59ef60dc4eae77414ec4b811d158331ae446ca
Ludovic Lasry <llasry@ludo-mac.home>
2017-02-18 17:10:04 +0100

    fix clean issue that remove _

--- lib/includes.php
+++ lib/includes.php
@@ -9,7 +9,7 @@ require_once(PROJECT_PATH.'/vendor/smarty/smarty/libs/Smarty.class.php');
 function clean($string) {
    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
-   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   return preg_replace('/[^A-Za-z0-9\_]/', '', $string); // Removes special chars.
 }
 
 /*
